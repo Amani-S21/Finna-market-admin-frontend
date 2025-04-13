99755e9b4c2f9c8eab71548acdc17b0e3c7f3303
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-04-13 22:50:28 +0200

    Implement radix ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Theme>{children}</Theme>
       </body>
     </html>
   );
