cc869f27c03fe79e45b323fd0c14ff60a0e9ad85
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-04-18 18:26:47 +0200

    Add schema for signin in

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Card, Flex, Link, TextField } from "@radix-ui/themes";
 import { signIn } from "next-auth/react";
 import { use } from "react";
+import { useForm } from "react-hook-form";
+import { SigninSchema, signinSchema } from "./types";
 
 interface Props {
   searchParams: Promise<{ callbackUrl: string }>;
@@ -11,10 +14,17 @@ interface Props {
 const SigninPage = ({ searchParams }: Props) => {
   const { callbackUrl } = use(searchParams);
 
-  const onSubmit = async () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<SigninSchema>({ resolver: zodResolver(signinSchema) });
+
+  const onSubmit = async (data: SigninSchema) => {
     await signIn("credentials", {
-      phone: "+243971945367",
-      password: "12345",
+      phone: data.phone,
+      password: data.password,
       redirect: true,
       callbackUrl: callbackUrl,
     });
@@ -22,30 +32,35 @@ const SigninPage = ({ searchParams }: Props) => {
 
   return (
     <Card className="drop-shadow-2xl">
-      <Flex
-        direction="column"
-        className="items-center min-w-sm p-4"
-      >
+      <Flex direction="column" className="items-center min-w-sm p-4">
         <p className="text-xl font-bold my-4">Login</p>
-        <div className="flex flex-col mt-6 w-full max-w-sm">
+        <form
+          className="flex flex-col mt-6 w-full max-w-sm"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className="flex flex-col space-y-2 mt-5">
             <p className="text-sm font-bold">Numero de téléphone</p>
-            <TextField.Root placeholder="Numero de téléphone" />
+            <TextField.Root
+              {...register("phone")}
+              placeholder="Numero de téléphone"
+            />
           </div>
           <div className="flex flex-col space-y-2 mt-5">
             <p className="text-sm font-bold">Mot de passe</p>
-            <TextField.Root type="password" placeholder="Mot de passe" />
+            <TextField.Root
+              {...register("password")}
+              type="password"
+              placeholder="Mot de passe"
+            />
           </div>
-          <Button mt="5" onClick={onSubmit}>
-            Enregistrer
-          </Button>
+          <Button mt="5">Enregistrer</Button>
           <span className="mx-auto mt-6 text-xs">
             Vous n'avez pas de compte
           </span>
           <span className="mx-auto mt-2 text-xs text-primary  cursor-pointer hover:underline">
             <Link href="/auth/signup">Créer en un</Link>
           </span>
-        </div>
+        </form>
       </Flex>
     </Card>
   );
---
+++ app/auth/signin/types.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const signinSchema = z.object({
+  phone: z.string(),
+  password: z
+    .string()
+    .min(5, "Le mot de passe doit etre au minimum 5 caracteres"),
+});
+
+export type SigninSchema = z.infer<typeof signinSchema>;
