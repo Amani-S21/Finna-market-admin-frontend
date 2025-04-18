3cada4d97fe18bd6f391d64b1cf0d6c84e7c0811
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-04-18 19:05:00 +0200

    Refactor: Create SigninForm

---
+++ app/auth/signin/_components/SigninForm.tsx
@@ -0,0 +1,60 @@
+import { SigninSchema } from "@/app/types";
+import { signinSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Text, TextField } from "@radix-ui/themes";
+import { signIn } from "next-auth/react";
+import { useForm } from "react-hook-form";
+
+const SigninForm = ({ callbackUrl }: { callbackUrl: string }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<SigninSchema>({ resolver: zodResolver(signinSchema) });
+
+  const onSubmit = async (data: SigninSchema) => {
+    await signIn("credentials", {
+      phone: data.phone,
+      password: data.password,
+      redirect: true,
+      callbackUrl,
+    });
+  };
+
+  return (
+    <form
+      className="flex flex-col mt-6 w-full max-w-sm"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex flex-col space-y-2 mt-5">
+        <p className="text-sm font-bold">Numero de téléphone</p>
+        <TextField.Root
+          {...register("phone")}
+          placeholder="Numero de téléphone"
+        />
+        {errors.phone && (
+          <Text color="red" size="1" as="p">
+            {errors.phone.message}
+          </Text>
+        )}
+      </div>
+      <div className="flex flex-col space-y-2 mt-5">
+        <p className="text-sm font-bold">Mot de passe</p>
+        <TextField.Root
+          {...register("password")}
+          type="password"
+          placeholder="Mot de passe"
+        />
+        {errors.password && (
+          <Text color="red" size="1" as="p">
+            {errors.password.message}
+          </Text>
+        )}
+      </div>
+      <Button mt="5">Enregistrer</Button>
+    </form>
+  );
+};
+
+export default SigninForm;
--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Card, Flex, Link, Text, TextField } from "@radix-ui/themes";
-import { signIn } from "next-auth/react";
+import { Card, Flex } from "@radix-ui/themes";
 import { use } from "react";
-import { useForm } from "react-hook-form";
-import { SigninSchema } from "../../types";
-import { signinSchema } from "../../validationSchemas";
+import SigninForm from "./_components/SigninForm";
 
 interface Props {
   searchParams: Promise<{ callbackUrl: string }>;
@@ -15,57 +11,11 @@ interface Props {
 const SigninPage = ({ searchParams }: Props) => {
   const { callbackUrl } = use(searchParams);
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors, isSubmitting },
-  } = useForm<SigninSchema>({ resolver: zodResolver(signinSchema) });
-
-  const onSubmit = async (data: SigninSchema) => {
-    await signIn("credentials", {
-      phone: data.phone,
-      password: data.password,
-      redirect: true,
-      callbackUrl: callbackUrl,
-    });
-  };
-
   return (
     <Card className="drop-shadow-2xl">
       <Flex direction="column" className="items-center min-w-sm p-4">
-        <p className="text-xl font-bold my-4 ">Login</p>
-        <form
-          className="flex flex-col mt-6 w-full max-w-sm"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className="flex flex-col space-y-2 mt-5">
-            <p className="text-sm font-bold">Numero de téléphone</p>
-            <TextField.Root
-              {...register("phone")}
-              placeholder="Numero de téléphone"
-            />
-            {errors.phone && (
-              <Text color="red" size="1" as="p">
-                {errors.phone.message}
-              </Text>
-            )}
-          </div>
-          <div className="flex flex-col space-y-2 mt-5">
-            <p className="text-sm font-bold">Mot de passe</p>
-            <TextField.Root
-              {...register("password")}
-              type="password"
-              placeholder="Mot de passe"
-            />
-            {errors.password && (
-              <Text color="red" size="1" as="p">
-                {errors.password.message}
-              </Text>
-            )}
-          </div>
-          <Button mt="5">Enregistrer</Button>
-        </form>
+        <p className="text-xl font-bold">Login</p>
+        <SigninForm callbackUrl={callbackUrl} />
       </Flex>
     </Card>
   );
