511c2b5fdefaa659c3746ac2556b24079af7a87c
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-04-18 20:53:37 +0200

    Add powered by text on signin page

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,21 +1,25 @@
 "use client";
 
-import { Card, Flex } from "@radix-ui/themes";
+import { Card, Flex, Text } from "@radix-ui/themes";
 import { use } from "react";
 import SigninForm from "./_components/SigninForm";
 
 type Props = {
   searchParams: Promise<{ callbackUrl: string }>;
-}
+};
 
 const SigninPage = ({ searchParams }: Props) => {
   const { callbackUrl } = use(searchParams);
 
   return (
-    <Card className="drop-shadow-2xl">
-      <Flex direction="column" className="items-center min-w-sm p-4">
-        <p className="text-xl font-bold">Login</p>
+    <Card className="drop-shadow-2xl w-full max-w-[400px] mx-4">
+      <Flex direction="column" className="items-center p-4 pb-8">
+        <p className="text-xl font-bold mt-4">Connexion</p>
+        <p className="text-xs my-2">
+          Veuillez entrer vos identifiants pour continuer
+        </p>
         <SigninForm callbackUrl={callbackUrl} />
+        <Text size="1" color="gray" my="4">Powered by ksoft</Text>
       </Flex>
     </Card>
   );
