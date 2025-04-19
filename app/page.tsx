1afeb9364a1aa21856569a8e652cf9ee2e8d7b93
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-04-19 12:08:59 +0200

    Implement signout button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
-"use client"
+"use client";
 
-import { Card, Grid } from "@radix-ui/themes";
+import { Button, Card, Grid, Heading } from "@radix-ui/themes";
+import { BadgeDollarSign, BedSingle, LogOut, Plane } from "lucide-react";
+import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
@@ -9,6 +11,15 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col container mx-auto pb-32">
       <div className="py-8 flex gap-4 items-center justify-end">
+        <Button
+          onClick={() => signOut()}
+          variant="soft"
+          radius="full"
+          color="red"
+        >
+          <LogOut size={15} />
+          <span className="text-xs">Déconnection</span>
+        </Button>
         <div className="h-[60px] w-[60px] rounded-full bg-white border border-gray-300 flex justify-center items-center">
           <p>GE</p>
         </div>
@@ -23,31 +34,34 @@ export default function Home() {
             className="hover:cursor-pointer hover:bg-gray-300"
             onClick={() => router.push("/market")}
           >
-            <p>Super marché</p>
-            <p className="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
-              magni excepturi consectetur perferendis iusto odio. Voluptates
-              aperiam facere, nihil porro ad at quidem rem quos nulla libero
-              natus, odio dolore!
-            </p>
+            <div className="flex flex-col items-center justify-center h-full gap-4 py-4 px-6">
+              <Heading size="3" color="gray">
+                Marché
+              </Heading>
+              <BadgeDollarSign size={50} color="gray" />
+            </div>
           </Card>
-          <Card className="hover:cursor-pointer hover:bg-gray-300">
-            <p>Super marché</p>
-            <p className="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
-              magni excepturi consectetur perferendis iusto odio. Voluptates
-              aperiam facere, nihil porro ad at quidem rem quos nulla libero
-              natus, odio dolore!
-            </p>
+          <Card
+            className="hover:cursor-pointer hover:bg-gray-300"
+            onClick={() => router.push("/market")}
+          >
+            <div className="flex flex-col items-center justify-center h-full gap-4 py-4 px-6">
+              <Heading size="3" color="gray">
+                Transport
+              </Heading>
+              <Plane size={50} color="gray" />
+            </div>
           </Card>
-          <Card className="hover:cursor-pointer hover:bg-gray-300">
-            <p>Super marché</p>
-            <p className="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
-              magni excepturi consectetur perferendis iusto odio. Voluptates
-              aperiam facere, nihil porro ad at quidem rem quos nulla libero
-              natus, odio dolore!
-            </p>
+          <Card
+            className="hover:cursor-pointer hover:bg-gray-300"
+            onClick={() => router.push("/market")}
+          >
+            <div className="flex flex-col items-center justify-center h-full gap-4 py-4 px-6">
+              <Heading size="3" color="gray">
+                Réservation
+              </Heading>
+              <BedSingle size={50} color="gray" />
+            </div>
           </Card>
         </Grid>
       </div>
