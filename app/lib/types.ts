f1dc9f31d0ba77af6cbbc3cc67d1877d1c646173
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-04-21 13:21:51 +0200

    Create products table component

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -35,3 +35,76 @@ export type User = {
   createdAt: string;
   updatedAt: string;
 };
+
+export type ProductResponse = {
+  count: number;
+  data: Product[];
+};
+
+export type Product = {
+  id: string;
+  name: string;
+  description: string;
+  oldPrice: number;
+  currentPrice: number;
+  purchasedPrice: number;
+  publised: boolean;
+  pictures: string[];
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+  subCategoryId: string;
+  user: User;
+  subCategory: SubCategory;
+  featuresAffectations: FeatureAffectation[];
+};
+
+export type SubCategory = {
+  id: string;
+  name: string;
+  categoryId: string;
+  createdAt: string;
+  updatedAt: string;
+  category: Category;
+};
+
+export type Category = {
+  id: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type FeatureAffectation = {
+  featureId: string;
+  createdAt: string;
+  updatedAt: string;
+  productId: string;
+  feature: Feature;
+  featuresAffectationsHasValues: FeatureAffectationValue[];
+};
+
+export type Feature = {
+  id: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type FeatureAffectationValue = {
+  price: number;
+  featureAffectationProductId: string;
+  featureAffectationFeatureId: string;
+  featureValueId: string;
+  createdAt: string;
+  updatedAt: string;
+  featureValue: FeatureValue;
+};
+
+export type FeatureValue = {
+  id: string;
+  value: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
--- app/market/products/_components/ProductsTable.tsx
+++ app/market/products/_components/ProductsTable.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+"use client";
 
-const ProductsTable = () => {
+import { formattedDate } from "@/app/lib/tools";
+import { ProductResponse } from "@/app/lib/types";
+import { IconButton, Table } from "@radix-ui/themes";
+import { useRouter } from "next/router";
+import React from "react";
+import { IoIosMore } from "react-icons/io";
+
+const ProductsTable = ({
+  productsResponse,
+}: {
+  productsResponse: ProductResponse;
+}) => {
+  const router = useRouter();
+  const columns: {
+    label: string;
+  }[] = [
+    { label: "N" },
+    { label: "Boutique" },
+    { label: "Addrèsse" },
+    { label: "Date" },
+    { label: "Action" },
+  ];
+
   return (
-    <div>ProductsTable</div>
-  )
-}
+    <Table.Root variant="surface">
+      <Table.Header>
+        <Table.Row>
+          {columns.map((column) => (
+            <Table.ColumnHeaderCell key={column.label}>
+              {column.label}
+            </Table.ColumnHeaderCell>
+          ))}
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {productsResponse?.data.map((product, index) => (
+          <Table.Row key={product.id}>
+            <Table.Cell>{index + 1}</Table.Cell>
+            <Table.Cell>{product.name}</Table.Cell>
+            <Table.Cell className="truncate max-w-[300px]">
+              {product.description}
+            </Table.Cell>
+            <Table.Cell>{formattedDate(product.createdAt)}</Table.Cell>
+            <Table.Cell>
+              <IconButton
+                variant="ghost"
+                ml="4"
+                onClick={() => router.push(`/market/shops/${product.id}`)}
+              >
+                <IoIosMore size={20} color="black" />
+              </IconButton>
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  );
+};
 
-export default ProductsTable
+export default ProductsTable;
