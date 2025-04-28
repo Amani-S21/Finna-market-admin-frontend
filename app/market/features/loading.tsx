275488cd4dfac1c48b0a690c9319d519661855c9
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-04-28 19:29:20 +0200

    Create features loading page

---
+++ app/market/features/loading.tsx
@@ -0,0 +1,13 @@
+import LoadingTable from "@/app/_components/LoadingTable";
+import FeaturesToolBar from "./_components/FeaturesToolBar";
+
+const LoadingFeatures = () => {
+  return (
+    <>
+      <FeaturesToolBar />
+      <LoadingTable />
+    </>
+  );
+};
+
+export default LoadingFeatures;
