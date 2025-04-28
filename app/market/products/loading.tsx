dfd7fa085a208c8bb099e752400cd455d97b65ee
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-04-28 19:15:00 +0200

    Extract loading table component

--- app/market/products/loading.tsx
+++ app/market/products/loading.tsx
@@ -1,52 +1,13 @@
-import { Skeleton, Table } from "@radix-ui/themes";
+import LoadingTable from "@/app/_components/LoadingTable";
 import { ProductsToolBar } from "./_components";
 
 const LoadingProductsPage = () => {
-  const products = [...Array(5)];
+  
 
   return (
     <div>
       <ProductsToolBar />
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>N</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Boutique</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Addrèsse</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Proprietaire</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Créé le</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Action</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {products.map((_, index) => (
-            <Table.Row key={index}>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-      <div className="flex gap-4">
-        <Skeleton className="mt-4 w-[100px]" />
-        <Skeleton className="mt-4 w-[100px]" />
-      </div>
+      <LoadingTable />
     </div>
   );
 };
--- app/market/shops/loading.tsx
+++ app/market/shops/loading.tsx
@@ -1,53 +1,12 @@
-import { Skeleton, Table } from "@radix-ui/themes";
+import LoadingTable from "@/app/_components/LoadingTable";
 import "react-loading-skeleton/dist/skeleton.css";
 import ShopsToolBar from "./_components/ShopsToolBar";
 
 const LoadingShopspPage = () => {
-  const shops = [...Array(5)];
-
   return (
     <div>
       <ShopsToolBar />
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>N</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Boutique</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Addrèsse</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Proprietaire</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Créé le</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Action</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {shops.map((_, index) => (
-            <Table.Row key={index}>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-              <Table.Cell>
-                <Skeleton />
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-      <div className="flex gap-4">
-        <Skeleton className="mt-4 w-[100px]" />
-        <Skeleton className="mt-4 w-[100px]" />
-      </div>
+      <LoadingTable />
     </div>
   );
 };
