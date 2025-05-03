8a9a35c1489cf822684829ecd9a9126e9ed4783d
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-05-03 11:32:05 +0200

    Use flex column on orders page

--- app/market/orders/_components/OrdersToolBar.tsx
+++ app/market/orders/_components/OrdersToolBar.tsx
@@ -3,12 +3,12 @@ import { AiOutlineOrderedList } from "react-icons/ai";
 
 const OrdersToolBar = () => {
   return (
-    <div className="mb-2">
+    <div>
       <div className="flex items-center space-x-4">
         <AiOutlineOrderedList />
         <span className="font-bold">Commandes</span>
       </div>
-      <Text as="p" size="2" mb="4">
+      <Text as="p" size="2">
         Toutes les commandes disponibles dans l'entreprise
       </Text>
     </div>
--- app/market/orders/list/page.tsx
+++ app/market/orders/list/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Pagination } from "@/app/_components";
 import useAxiosAuth from "@/app/lib/hooks/useAxiosAuth";
@@ -7,13 +7,13 @@ import { use } from "react";
 import { useFetchOrders } from "../_features/hooks";
 import LoadingOrdersPage from "./loading";
 import { OrdersTable, OrdersToolBar } from "../_components";
+import { Flex } from "@radix-ui/themes";
 
 const OrdersPage = ({
   searchParams,
 }: {
   searchParams: Promise<{ page: string }>;
 }) => {
-
   const { status } = useSession();
   const axios = useAxiosAuth();
   const { page } = use(searchParams);
@@ -27,9 +27,9 @@ const OrdersPage = ({
   if (isLoading || status === "loading") return <LoadingOrdersPage />;
 
   if (error) return;
-  
+
   return (
-    <>
+    <Flex direction="column" gap="4">
       <OrdersToolBar />
       {ordersResponse && <OrdersTable ordersResponse={ordersResponse} />}
       <Pagination
@@ -38,7 +38,7 @@ const OrdersPage = ({
         itemCount={ordersResponse?.count ?? 0}
         className="mt-4"
       />
-    </>
+    </Flex>
   );
 };
 
