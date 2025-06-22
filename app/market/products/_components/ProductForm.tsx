eedd40515d2b9c355c1f9767fdcc96316caf126f
Optimus ged <gedeonyalakuhanda@gmail.com>
2025-06-22 22:57:24 +0200

    Uncomment product images implementation

--- app/market/products/_components/ProductForm.tsx
+++ app/market/products/_components/ProductForm.tsx
@@ -18,7 +18,7 @@ import {
   TextArea,
   TextField,
 } from "@radix-ui/themes";
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import { useEffect, useRef, useState } from "react";
 import { Controller } from "react-hook-form";
@@ -35,6 +35,7 @@ import {
   useCreateProduct,
   useFetchCategories,
   useProductForm,
+  useSendProductsLinks,
   useUpdateProduct
 } from "../_features/hooks";
 
@@ -42,6 +43,8 @@ import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import { useFetchFeaturesByValue } from "../../features/_features/hooks";
 import FeaturesToPostTable from "../new/_components/FeaturesToPostTable";
+import { AxiosInstance } from "axios";
+import { uploadUrl } from "../_features/api";
 
 const ProductForm = ({ product }: { product?: Product }) => {
   const { data: session } = useSession();
@@ -78,11 +81,11 @@ const ProductForm = ({ product }: { product?: Product }) => {
     formState: { errors, isSubmitting },
   } = useProductForm({ product });
 
-  // const { mutateAsync: uploadProductPicture } = useMutation({
-  //   mutationFn: ({ axios, file }: { axios: AxiosInstance; file: File }) =>
-  //     uploadUrl(axios, file),
-  //   retry: 0,
-  // });
+  const { mutateAsync: uploadProductPicture } = useMutation({
+    mutationFn: ({ axios, file }: { axios: AxiosInstance; file: File }) =>
+      uploadUrl(axios, file),
+    retry: 0,
+  });
 
   useEffect(() => {
     if (product) {
@@ -166,11 +169,11 @@ const ProductForm = ({ product }: { product?: Product }) => {
     isSuccess: isUpdateSuccess,
   } = useUpdateProduct({ axios });
 
-  // const {
-  //   mutateAsync: sendProductLinks,
-  //   isSuccess: sendProductLinksSuccess,
-  //   isPending: isPendingSendingLinks,
-  // } = useSendProductsLinks({ axios });
+  const {
+    mutateAsync: sendProductLinks,
+    isSuccess: sendProductLinksSuccess,
+    isPending: isPendingSendingLinks,
+  } = useSendProductsLinks({ axios });
 
   const onSubmit = async (data: ProductSchema) => {
     // Post product
@@ -213,27 +216,27 @@ const ProductForm = ({ product }: { product?: Product }) => {
   };
 
   // upload pictures
-  // const uploadPictures = async () => {
-  //   if (productImageUrls.current.length < 3)
-  //     for (let i = 0; i < productImageFiles.current.length; i++) {
-  //       const data = await uploadProductPicture({
-  //         axios,
-  //         file: productImageFiles.current[i],
-  //       });
-  //       productImageUrls.current.push(`${data?.url}`);
-  //     }
+  const uploadPictures = async () => {
+    if (productImageUrls.current.length < 3)
+      for (let i = 0; i < productImageFiles.current.length; i++) {
+        const data = await uploadProductPicture({
+          axios,
+          file: productImageFiles.current[i],
+        });
+        productImageUrls.current.push(`${data?.url}`);
+      }
 
-  //   // Now we can send the uploaded pictures and update the product
-  //   await sendProductLinks({
-  //     id: `${createdProductData?.id}`,
-  //     pictures: [...productImageUrls.current],
-  //   });
-  // };
+    // Now we can send the uploaded pictures and update the product
+    await sendProductLinks({
+      id: `${createdProductData?.id}`,
+      pictures: [...productImageUrls.current],
+    });
+  };
 
   useEffect(() => {
     if (isCreateSuccess) {
       (async () => {
-        // await uploadPictures();
+        await uploadPictures();
         queryClient.invalidateQueries({ queryKey: ["products"] });
         queryClient.invalidateQueries({ queryKey: ["products-by-id"] });
 
@@ -244,24 +247,24 @@ const ProductForm = ({ product }: { product?: Product }) => {
     }
   }, [isCreateSuccess, createdProductData, router, createError, queryClient]);
 
-  // useEffect(() => {
-  //   if (isCreateSuccess) {
-  //     (async () => {
-  //       await uploadPictures(); // Wait for uploads
-  //     })();
-  //   }
-  // }, [isCreateSuccess]);
+  useEffect(() => {
+    if (isCreateSuccess) {
+      (async () => {
+        await uploadPictures(); // Wait for uploads
+      })();
+    }
+  }, [isCreateSuccess]);
 
-  // useEffect(() => {
-  //   if (sendProductLinksSuccess) {
-  //     (async () => {
-  //       queryClient.invalidateQueries({ queryKey: ["products"] });
-  //       queryClient.invalidateQueries({ queryKey: ["products-by-id"] });
-  //       toast.success(`Produit créé avec succès`);
-  //       router.back(); // Only navigate after everything finishes
-  //     })();
-  //   }
-  // }, [isCreateSuccess, createdProductData, router]);
+  useEffect(() => {
+    if (sendProductLinksSuccess) {
+      (async () => {
+        queryClient.invalidateQueries({ queryKey: ["products"] });
+        queryClient.invalidateQueries({ queryKey: ["products-by-id"] });
+        toast.success(`Produit créé avec succès`);
+        router.back(); // Only navigate after everything finishes
+      })();
+    }
+  }, [isCreateSuccess, createdProductData, router]);
 
   useEffect(() => {
     if (isUpdateSuccess) {
